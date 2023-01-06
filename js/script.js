cd3e351c33ee1be51b25700e00720a88b2561ca0
Minenkova <tatomeliya53@gmail.com>
2023-01-06 19:04:25 +0200

    homework9

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const getInputText = document.getElementsByClassName('buttons__add');
+const valueDivText = document.getElementsByTagName('p');
+const inputText = document.getElementsByClassName('div__text');
+const clearBtnText = document.getElementsByClassName('buttons__clear');
+const showPrimaryText = document.getElementsByClassName('text__input');
+console.log(valueDivText); 
+console.log(getInputText);
+console.log(inputText);
+console.log(showPrimaryText);
+
+getInputText[0].addEventListener('click', () =>{
+    valueDivText[0].innerText = inputText[0].value;
+
+    if (inputText[0].value === ''){
+        showPrimaryText[0].classList.add('text__input--primary');
+    }
+});
+
+clearBtnText[0].addEventListener('click', ()=>{
+    inputText[0].value = '';
+    valueDivText[0].innerText = '';
+});
+
+
+
