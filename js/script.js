dcd71918614ebded1f682cb525af6029d6c1b022
Minenkova <tatomeliya53@gmail.com>
2023-01-11 11:05:48 +0200

    corrected the remark

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ const valueDivText = document.getElementsByTagName('p');
 const inputText = document.getElementsByClassName('div__text');
 const clearBtnText = document.getElementsByClassName('buttons__clear');
 const showPrimaryText = document.getElementsByClassName('text__input');
-console.log(valueDivText); 
-console.log(getInputText);
-console.log(inputText);
-console.log(showPrimaryText);
+// console.log(valueDivText); 
+// console.log(getInputText);
+// console.log(inputText);
+// console.log(showPrimaryText);
 
 getInputText[0].addEventListener('click', () =>{
     valueDivText[0].innerText = inputText[0].value;
@@ -14,12 +14,13 @@ getInputText[0].addEventListener('click', () =>{
     if (inputText[0].value === ''){
         showPrimaryText[0].classList.add('text__input--primary');
     }
+    else {
+        showPrimaryText[0].classList.remove('text__input--primary');
+    }
 });
 
 clearBtnText[0].addEventListener('click', ()=>{
     inputText[0].value = '';
     valueDivText[0].innerText = '';
 });
-
-
 
